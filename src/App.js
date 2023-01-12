d4a1591c15250e8a187c8d7e38d9d28cff5d8b3e
Gabriel Coelho <coelho.tibbers@gmail.com>
2023-01-12 13:47:57 -0300

    feat: pagina de manutenção

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import imagemLegal from './imagemLegal.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          DEVELOP
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Esse é o site da juliana!</p>
+        <p>Está em manutenção...</p>
+        <img src={ imagemLegal } alt="imagemLegal" />
       </header>
     </div>
   );
