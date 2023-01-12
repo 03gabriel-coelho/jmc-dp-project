3089ef70892ad7bf20fb1676ffe6921753ab6f77
Gabriel Coelho <coelho.tibbers@gmail.com>
2023-01-12 12:19:33 -0300

    fix: teste ci

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Gabiru
+          DEVELOP
         </p>
         <a
           className="App-link"
