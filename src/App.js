6f48bf7a29d89aca97933fa81155a00fd14cd32d
Gabriel Coelho <coelho.tibbers@gmail.com>
2023-04-12 19:25:43 -0300

    feat: rotas implementadas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import imagemLegal from './imagemLegal.png';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Clients from './pages/clients';
+import Company from './pages/company';
+import Contact from './pages/contact';
+import Services from './pages/services';
+import NotFound from './pages/notFound';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Esse é o site da juliana!</p>
-        <p>Está em manutenção...</p>
-        <img src={ imagemLegal } alt="imagemLegal" />
-      </header>
+      <Header />
+      <Routes>
+        <Route index element={<Company />} />
+        <Route path="services" element={ <Services /> } />
+        <Route path="clients" element={ <Clients /> } />
+        <Route path="contact" element={ <Contact /> } />
+        <Route path="*" element={ <NotFound /> } />
+      </Routes>
     </div>
   );
 }
