a386d002f3ac04fb59da808af537e4545616fe36
Gabriel Henrique Coelho Freitas <94490310+03gabriel-coelho@users.noreply.github.com>
2025-01-22 08:07:16 -0300

    Ajusta estilos e estrutura da segunda seção: adiciona ícones, redefine tamanhos e melhora responsividade

--- src/components/second_session/index.tsx
+++ src/components/second_session/index.tsx
@@ -1,5 +1,8 @@
 import styles from "./index.module.css";
 import { IoIosArrowForward } from "react-icons/io";
+import { FaChess } from "react-icons/fa";
+import { GiAlarmClock } from "react-icons/gi";
+import { GiProfit } from "react-icons/gi";
 
 export default function SecondSection() {
   return (
@@ -18,9 +21,36 @@ export default function SecondSection() {
         <div className={styles.second_section_header_five}>
           LUCRO e SUCESSO da sua empresa
         </div>
-        <div className={styles.second_section_container_one}>c1</div>
-        <div className={styles.second_section_container_two}>c2</div>
-        <div className={styles.second_section_container_three}>c3</div>
+        <div className={styles.second_section_container_one}>
+          <div className={styles.second_section_container_header}>
+            <FaChess />
+            <h3>Estratégia</h3>
+          </div>
+          <p className={styles.second_section_container_text}>
+            Alinhando objetivos estratégicos, cultura e valores das empresas às
+            competências e motivações dos profissionais.
+          </p>
+        </div>
+        <div className={styles.second_section_container_two}>
+          <div className={styles.second_section_container_header}>
+            <GiAlarmClock />
+            <h3>Agilidade</h3>
+          </div>
+          <p className={styles.second_section_container_text}>
+            Alinhando objetivos estratégicos, cultura e valores das empresas às
+            competências e motivações dos profissionais.
+          </p>
+        </div>
+        <div className={styles.second_section_container_three}>
+          <div className={styles.second_section_container_header}>
+            <GiProfit />
+            <h3>Economia</h3>
+          </div>
+          <p className={styles.second_section_container_text}>
+            Redução de custos causados pela rotatividade ou baixa produtividade
+            por conta de uma contratação realizada de forma equivocada
+          </p>
+        </div>
       </div>
     </section>
   );
