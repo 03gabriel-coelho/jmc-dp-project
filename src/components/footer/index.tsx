ce0d1786728aacc95fad17a14e67ed5fe3bd7fe4
Gabriel Henrique Coelho Freitas <94490310+03gabriel-coelho@users.noreply.github.com>
2025-03-07 12:35:48 -0300

    feat(footer): adiciona ícones de contato e ajusta formatação do footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,13 +1,15 @@
 import styles from "./index.module.css";
 import { LuInstagram } from "react-icons/lu";
 import { FaFacebookSquare } from "react-icons/fa";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaPhoneVolume } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <h3>Contagem, Minas Gerais</h3>
+      <h3><FaMapMarkerAlt />Contagem, Minas Gerais</h3>
       <div className={styles.contact}>
-        <h3>Entre em contato já!</h3>
+        <h3><FaPhoneVolume />Entre em contato já!</h3>
         <p>+55 31 99849-2779</p>
         <p>+55 31 3395-2023</p>
         <p>jmcconsultrh@gmail.com </p>
